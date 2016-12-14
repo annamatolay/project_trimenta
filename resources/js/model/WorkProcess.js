c3ff68fef4241a6b92b4c4fbdb1d3c933d926aa9
Codecool-Palko <matolaypal@gmail.com>
2016-12-14 14:46:21 +0100

    implement attributes of model, getDatum, load, save functions

--- resources/js/model/WorkProcess.js
+++ resources/js/model/WorkProcess.js
@@ -2,25 +2,48 @@
  * Created by Matolay Pál
  */
 
-function  workProcess(name, excepted, datum) {
-    this.name = name;
-    this.excepted = excepted;
+function  WorkProcess() {
+    var allProcess = null;
+
+    this.name = null;
+    this.excepted = null;
     this.quantity = 0;
-    this.datum = datum;
 
-    this.plusQuantity = function () {
-        return this.quantity++;
+    this.getDatum = function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        }
+
+        if(mm<10) {
+            mm='0'+mm
+        }
+
+        return yyyy+'.'+mm+'.'+dd;
     };
 
-    this.checkQuantity = function () {
-      return this.quantity>=this.excepted;
+    this.datum = this.getDatum();
+
+    this.load = function () {
+        var json = new JsonCtrl("get");
+        return json.work("work");
     };
 
-    this.saveProcess = function () {
-        jsonController("set", "work");
+    this.save = function () {
+        allProcess = this.load();
+        if (!allProcess) {
+            allProcess = [];
+        }
+        this.name = $("input[name=workName]").val();
+        this.excepted = $("input[name=expected]").val();
+        allProcess.push(this);
+        var json = new JsonCtrl("set");
+        json.setObject(allProcess);
+        json.work("work");
     };
 
-    this.loadProgress = function () {
-        getJson("get", "work");
-    }
 }
