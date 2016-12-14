072cf58c1908f52c273f43f67d7a6e4c675fb479
Codecool-Palko <matolaypal@gmail.com>
2016-12-14 14:40:22 +0100

    can add and show the work process

--- resources/js/controller/WorkController.js
+++ resources/js/controller/WorkController.js
@@ -1,3 +1,29 @@
 /**
- * Created by codecool-palko on 2016.12.12..
+ * Created by Matolay Pál
  */
+
+function WorkController() {
+    $("#newWBut").click(function () {
+        $("#newWorkProc").show();
+    });
+
+    var workProcess = new WorkProcess();
+
+    this.addWorkProcess = function () {
+        $("#workSaveBut").click(function () {
+            workProcess.save();
+            location.reload();
+        })
+    };
+
+    this.showDailyProcess = function () {
+        $("#dailyProcBut").click(function () {
+            var data = workProcess.load();
+            console.log(data);
+            for (var i=0;i<data.length;i++) {
+                console.log("quantity: "+data[i].quantity+" name: "+data[i].name);
+                $("#workProc").append(data[i].quantity+'<button>'+data[i].name+'</button>')
+            }
+        })
+    }
+}
