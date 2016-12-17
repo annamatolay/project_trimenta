0cd7b0752c237fc5c6b87398a155fd5924b65f59
Codecool-Palko <matolaypal@gmail.com>
2016-12-17 10:47:55 +0100

    modal contet, button created dynamically based on jsons data

--- resources/js/controller/WorkController.js
+++ resources/js/controller/WorkController.js
@@ -4,7 +4,12 @@
 
 function WorkController() {
     $("#newWBut").click(function () {
+        $("#workProc").hide();
         $("#newWorkProc").show();
+    });
+
+    $("#dailyProcBut").click(function () {
+        $("#newWorkProc").hide();
     });
 
     var workProcess = new WorkProcess();
@@ -17,12 +22,57 @@ function WorkController() {
     };
 
     this.showDailyProcess = function () {
-        $("#dailyProcBut").click(function () {
-            var data = workProcess.load();
-            console.log(data);
-            for (var i=0;i<data.length;i++) {
-                $("#workProc").append(data[i].quantity+'<button>'+data[i].name+'</button>')
+        var data = workProcess.load();
+
+        console.log(data);
+
+        if(!data) {
+            return;
+        }
+
+        for (var i=0;i<data.length;i++) {
+            var excepted = parseInt(data[i].excepted);
+            var quantity = parseInt(data[i].quantity);
+            var color = null;
+
+            if (excepted>quantity) {
+                color = "btn btn-danger";
+            } else {
+                color = "btn btn-success";
             }
-        })
-    }
+
+            $("#workProc").append(quantity +
+                '<button type="button" class="'+color+'"  value="'+i+
+                '" data-toggle="modal" data-target="#myModal">' + data[i].name+'</button>');
+        }
+
+        $(document).on("click", "button", function(){
+            if(this.value) {
+                var index = parseInt(this.value);
+                $("#modalTitle").empty().append('<h4 class="modal-title">Munkafolyamat neve: '+data[index].name+'</h4>');
+                $("#modalBody").empty().append(
+                    "Létrehozás Dátuma: "+data[index].datum+"<br>"+
+                    "Elvárt mennyiség: "+data[index].excepted);
+                $("#modalFooter").empty().append('<button id="saveQuantBut" type="button" class="btn btn-default" ' +
+                    'data-dismiss="modal" value="'+index+'">Mentés</button>')
+            }
+        });
+
+        $("#dailyProcBut").click(function () {
+            $("#workProc").show()
+        });
+    };
+
+    this.saveWorkQuantity = function () {
+        var allProcess = workProcess.load();
+        $(document).on("click", "#saveQuantBut", function(){
+            var index = parseInt(this.value);
+            allProcess[index].quantity+=1;
+            console.log(allProcess[index]);
+            var json = new JsonCtrl("set");
+            json.setObject(allProcess);
+            json.work("work");
+            location.reload();
+        });
+    };
 }
